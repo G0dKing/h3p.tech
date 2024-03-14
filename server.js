78bdc76fadb3bc849219282e13bfd593a0a3caa9
Alex Pariah <68952952+G0dKing@users.noreply.github.com>
2024-03-14 00:04:35 +0000

    changed express port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express=require('express')
 const path=require('path')
 const cors=require('cors')
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 3000
 
 const app=express()
 
