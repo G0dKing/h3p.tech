0659b4ecf46945416541120b2c769369ddd32512
G0dKing <ajcaskey@gmail.com>
2024-03-14 06:56:26 -0400

    Reconfigured Backend

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
+// server.js
+
+// Imports
 const express=require('express')
 const path=require('path')
 const cors=require('cors')
 
-const port = process.env.PORT || 3000
-
+// Initialize Express.js
 const app=express()
 
-app.use(cors)
-app.use(express.static(path.join(__dirname, 'client/dist')))
+//Middleware
+app.use(cors())
 
+// Serve Static Build
+app.use(express.static(path.join(__dirname, 'client', 'dist')));
+
+// Fallback
 app.get('*',(req, res) => {
-    res.sendFile(path.join(__dirname, 'client/dist/index.html'))
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 })
 
-app.listen(port)
-console.log(`Server Listening on Port ${port}`)
+// Deployment
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+})
